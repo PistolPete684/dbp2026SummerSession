dffd0ad711349f3dff1c55aef38290dc5f4971f1
Peter Linton <pwlinton@mta.ca>
2026-06-20 11:15:16 -0300

    Initial Commit

---
+++ dbp2026HelloWorld/dbp2026HelloWorld/dbp2026HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dbp2026HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
