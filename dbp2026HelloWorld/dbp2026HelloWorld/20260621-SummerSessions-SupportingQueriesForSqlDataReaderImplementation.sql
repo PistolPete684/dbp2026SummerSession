3aa02d8509e7f3ac707cb16f5e992ea8f109405f
Peter Linton <pwlinton@mta.ca>
2026-06-21 10:36:54 -0300

    Added: 20260621-SummerSessions-SupportingQueriesForSqlDataReaderImplementation

---
+++ dbp2026HelloWorld/dbp2026HelloWorld/20260621-SummerSessions-SupportingQueriesForSqlDataReaderImplementation.sql
@@ -0,0 +1,12 @@
+  
+/*
+    NAME:   Peter Linton
+    DATE:   2026-06-20
+    CLASS:  Summer Project
+    PURPOSE:Establish Connection to Northwind DB
+*/
+
+
+SELECT * FROM Customers ;
+
+SELECT * FROM Customers ;
