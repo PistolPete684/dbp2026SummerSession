e52f07fea0f5e0734468ecf1ce6e4cb15ba01faa
Peter Linton <pwlinton@mta.ca>
2026-06-30 09:39:01 -0300

    Added dbp2026Demo2

---
+++ dbp2026Demo2/dbp2026DataTableAdapterDemo/dbp2026DataTableAdapterDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dbp2026DataTableAdapterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
