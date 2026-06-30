eba8bb4d2dbfbe75351e257e3b8882de0477a343
Peter Linton <pwlinton@mta.ca>
2026-06-30 10:57:35 -0300

    created the connection object

--- dbp2026Demo2/dbp2026DataTableAdapterDemo/dbp2026DataTableAdapterDemo/Program.cs
+++ dbp2026Demo2/dbp2026DataTableAdapterDemo/dbp2026DataTableAdapterDemo/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace dbp2026DataTableAdapterDemo
+﻿using System.Data.SqlClient;
+
+namespace dbp2026DataTableAdapterDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! Let's  interact with the DB using Data Tables, Sets and Adapters");
+
+            string connectionString = "Database=Northwind; Integrated Security=SSPI; Server=LITTLE-JOHN;" ;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            Console.WriteLine("Connection successfully Established!");
+
+            connection.Close();
         }
     }
 }
